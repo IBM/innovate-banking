65a96a4fc9303af4e574057b43f322b16c4b5085
Stefan Momm <stefan.momm@ventura-digital.de>
2021-06-21 16:52:06 +0200

    Change accordion behavior

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -1,90 +1,22 @@
-import RichText from '@/components/RichText';
-import { ChevronDown32, ChevronUp32 } from '@carbon/icons-react';
-import { Column, Grid, Row } from 'carbon-components-react';
-import clsx from 'clsx';
-import dynamic from 'next/dynamic';
+import { Grid, Row } from 'carbon-components-react';
 import PropTypes from 'prop-types';
-import React, { createRef, useState } from 'react';
-import AnimateHeight from 'react-animate-height';
+import React from 'react';
+import AccordionItem from '../AccordionItem';
 import Styles from './styles.module.scss';
 
 const Accordion = ({ items }) => {
-	const [activeItemIndex, setActiveItemIndex] = useState(null);
-
-	const contentRefs = [];
-
-	const onHeaderClick = (event, index) => {
-		event.preventDefault();
-		setActiveItemIndex(index === activeItemIndex ? null : index);
-	};
-
 	return (
 		<Grid className={Styles.Accordion}>
-			<Row className={Styles.AccordionList}>
+			<Row>
 				{items.map((item, index) => {
-					const IconComponent = dynamic(() => import('@carbon/pictograms-react').then(mod => mod[item.icon]));
-
-					const contentRef = createRef(null);
-					contentRefs.push(contentRef);
-
 					return (
-						<Column
-							sm={4}
-							md={{
-								span: 6,
-								offset: 1,
-							}}
-							lg={{
-								span: 12,
-								offset: 2,
-							}}
-							className={clsx(Styles.AccordionListItem)}
+						<AccordionItem
 							key={index}
-							onClick={event => onHeaderClick(event, index)}
-						>
-							<div className={Styles.AccordionContent}>
-								<Row>
-									<Column
-										sm={4}
-										md={{
-											span: 6,
-											offset: 1,
-										}}
-										lg={{ span: 14, offset: 1 }}
-									>
-										<Row>
-											<Column sm={4} md={8} lg={2}>
-												<IconComponent className={Styles.Icon} />
-											</Column>
-											<Column
-												sm={4}
-												md={8}
-												lg={{ offset: 1, span: 13 }}
-												className={Styles.AccordionHeader}
-											>
-												<RichText content={item.headline} className={Styles.RichTextHeading} />
-												{index === activeItemIndex && (
-													<ChevronUp32 className={Styles.OpenCloseIcon} />
-												)}
-												{index !== activeItemIndex && (
-													<ChevronDown32 className={Styles.OpenCloseIcon} />
-												)}
-											</Column>
-										</Row>
-										<Row>
-											<Column sm={4} md={7} lg={{ offset: 3, span: 13 }}>
-												<AnimateHeight
-													duration={500}
-													height={index === activeItemIndex ? 'auto' : 0}
-												>
-													<RichText content={item.content} className={Styles.RichText} />
-												</AnimateHeight>
-											</Column>
-										</Row>
-									</Column>
-								</Row>
-							</div>
-						</Column>
+							icon={item.icon}
+							headline={item.headline}
+							content={item.content}
+							isOpenDefault={index === 0}
+						/>
 					);
 				})}
 			</Row>
