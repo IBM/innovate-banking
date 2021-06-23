53784b9aaa4e7d521851df44a41ff1ff23fafd57
Stefan Momm <stefan.momm@ventura-digital.de>
2021-06-23 13:47:14 +0200

    Fix typo

--- src/data/page/risk-compliance.js
+++ src/data/page/risk-compliance.js
@@ -289,7 +289,7 @@ module.exports = req => {
 					},
 					titles: {
 						left:
-							'<p class="d1">Let’!</p><p class="p" style="max-width: 200px;">Fragen oder Interesse an weiteren Informationen?<br />Sprechen Sie uns an!</p>',
+							'<p class="d1">Let’s&nbsp;talk!</p><p class="p" style="max-width: 200px;">Fragen oder Interesse an weiteren Informationen?<br />Sprechen Sie uns an!</p>',
 					},
 					fullWidth: true,
 					children: [
