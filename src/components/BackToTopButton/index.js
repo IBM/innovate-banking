c8817aed2a952bcb414b4a36313a7d0e2213f469
Stefan Momm <stefan.momm@ventura-digital.de>
2022-02-20 22:38:34 +0100

    feat: adjust back to top button

--- src/components/BackToTopButton/index.js
+++ src/components/BackToTopButton/index.js
@@ -1,4 +1,3 @@
-import { UpToTop24 } from '@carbon/icons-react'
 import React, { useEffect, useState } from 'react'
 import { CSSTransition } from 'react-transition-group'
 import Styles from './styles.module.scss'
@@ -23,7 +22,12 @@ const BackToTopButton = () => {
   return (
     <CSSTransition in={isVisible} timeout={200} classNames="back-to-top">
       <a className={Styles.Button} href="#top">
-        <UpToTop24 className={Styles.Icon} />
+        <svg width={24} height={24}>
+          <path
+            d="M1.714 0h20.572v1.714H1.714V0ZM3.43 12l1.208 1.208 6.506-6.496V24h1.714V6.712l6.506 6.496L20.572 12 12 3.429 3.429 12Z"
+            fill="#00539a"
+          />
+        </svg>
       </a>
     </CSSTransition>
   )
