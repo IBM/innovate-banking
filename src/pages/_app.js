fc6271c96bddaa1404860bc1711270d9d22c23ba
Stefan Momm <stefan.momm@ventura-digital.de>
2022-02-22 17:34:29 +0100

    feat: use next/script for IBM analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,42 @@
-/* eslint-disable react/prop-types */
-
 import '@/styles/reset.css'
 import '@/styles/globals.scss'
 import '@/styles/carbon-theme.scss'
+import Script from 'next/script'
 
-function MyApp({ Component, pageProps, pageData }) {
-  return <Component {...pageProps} pageData={pageData} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        id="ibm-analytics-settings"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window._ibmAnalytics = {
+                settings: {
+                  name: "INNOVATE_BANKING",
+                  isSpa: false,
+                  tealiumProfileName: "ibm-web-app"
+                }
+              };
+              digitalData = {
+                page: {
+                  pageInfo: {
+                    ibm: {
+                      siteId: "IBM_" + _ibmAnalytics.settings.name,
+                    }
+                  },
+                  category: {
+                    primaryCategory: "PC100"
+                  }
+                }
+              };
+              `,
+        }}
+      />
+      <Script strategy="afterInteractive" src="//1.www.s81c.com/common/stats/ibm-common.js" async />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
